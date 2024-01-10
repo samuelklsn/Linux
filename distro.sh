aee4b5ea4837c9b0609b7e627cdcf536a09285d1
Samuel Klassen <111987489+samuelklsn@users.noreply.github.com>
2024-01-10 17:32:36 +0100

    Update distro.sh

--- distro.sh
+++ distro.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt upgrade
 sudo apt install curl
-sudo apt install
+sudo apt install git
 flatpak install flathub com.google.Chrome
 flatpak install flathub com.discordapp.Discord 
