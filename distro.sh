8856456cf512c86c0eef2be0fad1f16d68d30f9f
Samuel Klassen <111987489+samuelklsn@users.noreply.github.com>
2024-01-10 17:42:05 +0100

    Update distro.sh

--- distro.sh
+++ distro.sh
@@ -4,3 +4,10 @@ sudo apt install curl
 sudo apt install git
 flatpak install flathub com.google.Chrome
 flatpak install flathub com.discordapp.Discord 
+flatpak install flathub org.mozilla.Thunderbird
+flatpak install flathub org.gimp.GIMP
+flatpak install flathub com.brave.Browser
+flatpak install flathub com.visualstudio.code
+flatpak install flathub org.gnome.Boxes
+flatpak install flathub com.mattjakeman.ExtensionManager
+flatpak install flathub io.github.mimbrero.WhatsAppDesktop
